84b1603820c728849d8252b52ad97a43dbbe370f
Shashank Kumar Srivastava <srivastavashashank033@gmail.com>
2024-01-19 00:14:38 +0530

    minor changes

--- chatzo/lib/feature/welcome_page/welcome_page.dart
+++ chatzo/lib/feature/welcome_page/welcome_page.dart
@@ -1,3 +1,4 @@
+import 'package:chatzo/common/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 class WelcomePage extends StatelessWidget {
@@ -6,15 +7,21 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF111B21),
+      backgroundColor: Coloors.backgroundDark,
       body:Column(
         children: [
           Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal:50),
-                child: Image.asset(
-                  'assets/images/circle.png',
-                  color: const Color(0xFF00A884),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal:50,
+                      vertical:10
+                  ),
+                  child: Image.asset(
+                    'assets/images/circle.png',
+                    color: Coloors.greenDark,
+                  ),
                 ),
               ),
           ),
@@ -37,14 +44,14 @@ class WelcomePage extends StatelessWidget {
                         text: const TextSpan(
                           text: "Read Our",
                           style: TextStyle(
-                            color: Color(0xFF8696A0),
+                            color: Coloors.greyDark,
                             height:1.5,
                           ),
                           children: [
                             TextSpan(
                               text: " Privacy Policy",
                               style:TextStyle(
-                                color: Color(0xFF53BDEB),
+                                color: Coloors.blueDark,
                               ),
                             ),
                             TextSpan(
@@ -61,20 +68,49 @@ class WelcomePage extends StatelessWidget {
                       height: 42,
                       width:MediaQuery.of(context).size.width - 200,
                       child: ElevatedButton(
-                      onPressed: (){},
-                      style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xFF00A884),
-                      foregroundColor: Color(0xFF111B21),
-                      splashFactory: NoSplash.splashFactory,
-                      elevation: 2,
-                      shadowColor: Colors.transparent,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0), // Set your desired radius here
-                      ),
-                      ),
-                      child: const Text("Agree and continue"),
+                            onPressed: (){},
+                            style: ElevatedButton.styleFrom(
+                            backgroundColor: Coloors.greenDark,
+                            foregroundColor: Coloors.backgroundDark,
+                            splashFactory: NoSplash.splashFactory,
+                            elevation: 2,
+                            shadowColor: Colors.transparent,
+                        ),
+                        child: const Text("Agree and continue"),
                    ),
                     ),
+                  const SizedBox(height:50),
+                  Material(
+                    color:Color(0xff182229),
+                    borderRadius: BorderRadius.circular(20),
+                    child: InkWell(
+                      onTap: (){},
+                      borderRadius: BorderRadius.circular(20),
+                      splashFactory: NoSplash.splashFactory,
+                      highlightColor: const Color(0xFF09141A),
+                      child: const Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children:[
+                          Icon(
+                            Icons.language,
+                            color: Coloors.greyDark,
+                          ),
+                          const SizedBox(width:15),
+                          Text(
+                              "English",
+                            style:TextStyle(
+                              color: Coloors.greyDark,
+                            ),
+                          ),
+                          const SizedBox(width:15),
+                          Icon(
+                              Icons.keyboard_arrow_down,
+                              color: Coloors.greenDark,
+                          ),
+                        ]
+                      )
+                    )
+                  ),
                 ],
               ),
             ),
